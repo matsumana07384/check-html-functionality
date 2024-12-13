22c7c39d2d9c1b3e9054f333ca7ca9e5ed9a00c2
matsumana07384 <manami.m.227@outlook.jp>
2024-12-13 15:04:43 +0900

    feat:リンククリック時に移動できるかを検証

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
-window.addEventListener('visibilitychange', () => {
-  if (document.visibilityState === 'visible') {
-    alert('active');
-  }
-})
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOMの読み込みが完了しました');
+
+  const link = document.getElementById('phoneLink');
+  link?.addEventListener('click', function() {
+    console.log('押下を検知')
+    setTimeout(function() {
+      window.location.href = '#targetSection';
+    }, 1000);
+  });
+
+  window.addEventListener('visibilitychange', () => {
+    if (document.visibilityState === 'visible') {
+      alert('active');
+    }
+  })
+});
