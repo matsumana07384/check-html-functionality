bc6095fb65bd6cc537c574d4a2615f9f49bba326
matsumana07384 <manami.m.227@outlook.jp>
2024-12-13 12:20:52 +0900

    rename:html file

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'main.html'));
 });
 
 app.listen(port, () => {
